235c342ae26e97e2ad228c0510595d519e2a258f
Ainsel <15658969+myAinsel@users.noreply.github.com>
2023-10-25 22:23:05 -0500

    Add VSCode settings

---
+++ vscode_install_extensions.sh
@@ -0,0 +1,7 @@
+code --install-extension cval.pastel-theme
+code --install-extension eamodio.gitlens
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-vscode.cpptools
+code --install-extension platformio.platformio-ide
+code --install-extension quangogage.pastel-pixels
